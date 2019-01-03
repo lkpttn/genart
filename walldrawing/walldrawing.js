ec874c73ab0d7b96ad2c66ebe79d224bb1376743
Luke Patton <lpatton@dosomething.org>
2019-01-03 15:59:03 -0500

    Initial commit

---
+++ walldrawing/walldrawing.js
@@ -0,0 +1,84 @@
+const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+
+const settings = {
+  dimensions: [2048, 2048],
+};
+
+const sketch = () => {
+  // Make 6x6 grid
+  const createGrid = () => {
+    const points = [];
+    const count = 6;
+
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+
+        points.push([u, v]);
+      }
+    }
+    return points;
+  };
+
+  const createLines = () => {
+    // An array of objects that contains each line
+    const lines = [];
+    const count = points.length;
+
+    for (let x = 0; x < count; x++) {
+      // Pick two random points on the grid
+      // Add them as a line to the array and remove them from the array of unused points
+      const firstPoint = random.pick(points);
+      const firstIndex = points.indexOf(firstPoint);
+      points.splice(firstIndex, 1);
+
+      const secondPoint = random.pick(points);
+      const secondIndex = points.indexOf(secondPoint);
+      points.splice(secondIndex, 1);
+
+      lines.push({ firstPoints: firstPoint, secondPoints: secondPoint });
+    }
+    return lines;
+  };
+  const points = createGrid();
+  const lines = createLines();
+  const margin = 300;
+
+  // Form a trapezoid with two parallel sides extending down
+  // Fill the trapezoid with a color, then stroke with the bg color
+  // Find a different set of points and repeat
+  // Layer the shapes by average Y position of their two grid points (taller in the back)
+
+  // Render
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    // Draw the lines
+    lines.forEach(data => {
+      // Destructure the data parameter so we can access the grid object properties
+      const { firstPoints, secondPoints } = data;
+      const [u1, v1] = firstPoints;
+      const [u2, v2] = secondPoints;
+
+      const x1 = lerp(margin, width - margin, u1);
+      const y1 = lerp(margin, height - margin, v1);
+
+      const x2 = lerp(margin, width - margin, u2);
+      const y2 = lerp(margin, height - margin, v2);
+
+      context.strokeStyle = 'black';
+      context.save();
+      context.beginPath();
+      context.moveTo(x1, y1);
+      context.lineTo(x2, y2);
+      context.stroke();
+      context.restore();
+    });
+  };
+};
+
+canvasSketch(sketch, settings);

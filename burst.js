ba1a5f0357242da2caaba06e90b20debd52b3a4e
Luke Patton <lpatton@dosomething.org>
2019-01-08 15:09:06 -0500

    Tweaking the burst to have colors and fewer nodes
    Also took it away a bit from the pixel based calculations

--- burst.js
+++ burst.js
@@ -1,6 +1,7 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes');
 
 const settings = {
   dimensions: [2048, 2048],
@@ -10,6 +11,8 @@ const sketch = () => {
   // Variables
   const margin = 300;
   const targets = [];
+  const radius = 0.01;
+  const palette = random.pick(palettes);
 
   // Generate grid
   const createGrid = () => {
@@ -29,15 +32,14 @@ const sketch = () => {
 
   const points = createGrid();
 
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 180; i++) {
     targets.push(random.pick(points));
   }
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = random.pick(palette);
     context.fillRect(0, 0, width, height);
     context.strokeStyle = 'black';
-    context.fillStyle = 'black';
 
     targets.forEach(points => {
       const u = points[0];
@@ -46,15 +48,18 @@ const sketch = () => {
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
+      const color = random.pick(palette);
+
       context.beginPath();
       context.moveTo(width / 2, height / 2);
       context.lineTo(x, y);
-      context.lineWidth = 0.1 + u;
+      context.lineWidth = (radius * width) / 8;
+      context.strokeStyle = color;
       context.stroke();
 
       context.beginPath();
-      // context.arc(x, y, 0.003 * width, 0, Math.PI * 2, false);
-      context.fillStyle = 'rgba(0,0,0,0.1)';
+      context.arc(x, y, (radius * width) / 4, 0, Math.PI * 2, false);
+      context.fillStyle = color;
       context.fill();
     });
   };

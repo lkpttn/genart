15fc97ba25a113fda8c3f7817e7eae707f961c6f
Luke Patton <lpatton@dosomething.org>
2019-01-08 12:12:19 -0500

    Moring experiments with walking and sand drawing

--- walk.js
+++ walk.js
@@ -6,9 +6,8 @@ const settings = {
   // Enable an animation loop
   animate: true,
   dimensions: [512, 512],
-  duration: 4,
   playbackRate: 'throttle',
-  fps: 24,
+  fps: 4,
 };
 
 canvasSketch(() => {
@@ -18,50 +17,71 @@ canvasSketch(() => {
   const margin = 100;
   let drift = 0;
 
-  // This is only returning one random value per run, duh
-  let randomY = random.range(margin, 512 - margin);
+  // We want an infinite animation where a dot moves to a new location randomly
+  // Randomly generate new coordinates
+  // Interpolate to the new coordinate over 1 second
+  // Choose a new destination
+  // Interpolate and repeat
 
   // Render
-  return ({ context, width, height, playhead }) => {
-    // Everything in here will be rendered 24 times per second
+  return ({ context, width, height, time }) => {
+    // Everything in here will be requested fps times per second
 
-    const position = {
-      oldPoints: [width / 2, 100],
-      newPoints: [width / 2, 300],
+    // Returns a random set of coordinates when called
+    const generateCoordinates = () => {
+      const randomX = random.range(margin, width - margin);
+      const randomY = random.range(margin, height - margin);
+      return [randomX, randomY];
     };
 
-    // Renew the white bg every frame
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+    const draw = () => {
+      const { oldPoints, newPoints } = position;
+      let [u1, v1] = oldPoints;
+      let [u2, v2] = newPoints;
 
-    // Grab random xy coodinates
-    const { oldPoints, newPoints } = position;
-    let [u1, v1] = oldPoints;
-    const [u2, v2] = newPoints;
+      // Renew the white bg every frame
+      context.fillStyle = 'white';
+      // context.fillRect(0, 0, width, height);
+      context.stokeStyle = 'black';
+
+      // Old location circle
+      context.beginPath();
+      context.arc(u1, v1, 5, 0, Math.PI * 2, false);
+      context.stroke();
+      context.closePath();
+
+      // Line between points
+      context.beginPath();
+      context.moveTo(u1, v1);
+      context.lineTo(u2, v2);
+      context.stroke();
+
+      // New points circle
+      context.beginPath();
+      context.arc(u2, v2, 5, 0, Math.PI * 2, false);
+      context.stroke();
+      context.fill();
+      context.closePath();
+    };
+
+    // Get some coordnates at the beginning of the sketch
+    // We don't want to set a new position every frame
+    const position = {
+      oldPoints: [width / 2, height / 2],
+      newPoints: generateCoordinates(),
+    };
+
+    // Draw a singular line between the new and old coordinates
+    draw();
 
     // Gather points inbetween old and new coordinates
-    const x = lerp(u1, u2, playhead);
-    const y = lerp(v1, v2, playhead);
-
-    context.stokeStyle = 'black';
-    context.fillStyle = 'black';
-
-    // Old location circle
-    context.beginPath();
-    context.arc(u1, v1, 5, 0, Math.PI * 2, false);
-    context.stroke();
-    context.closePath();
-
-    // New location circle
-    context.beginPath();
-    context.arc(u2, v2, 5, 0, Math.PI * 2, false);
-    context.stroke();
-    context.closePath();
+    // const x = lerp(u1, u2, time);
+    // const y = lerp(v1, v2, time);
 
     // Moving circle
-    context.beginPath();
-    context.arc(x, y, 5, 0, Math.PI * 2, false);
-    context.fill();
-    context.closePath();
+    // context.beginPath();
+    // context.arc(x, y, 5, 0, Math.PI * 2, false);
+    // context.fill();
+    // context.closePath();
   };
 }, settings);

b5d67e619cc55783db199163ba7266890eb23fe9
Luke <lkpttn@gmail.com>
2019-01-09 21:48:56 -0500

    Successfully created a random walk doodler

--- walk.js
+++ walk.js
@@ -6,86 +6,78 @@ const settings = {
   // Enable an animation loop
   animate: true,
   dimensions: [512, 512],
-  playbackRate: 'throttle',
   fps: 24,
 };
 
-canvasSketch(() => {
+canvasSketch(({ context, width, height }) => {
   // https://inconvergent.net/2016/shepherding-random-numbers/#introduction
   // Causing randomized drifts of nodes
 
   // https://codepen.io/DonKarlssonSan/post/random-walk
   // Random walker explanation
-  
-  const nodes = 17;
-  const margin = 100;
-  let drift = 0;
 
-  // We want an infinite animation where a dot moves to a new location randomly
-  // Randomly generate new coordinates
-  // Interpolate to the new coordinate over 1 second
-  // Choose a new destination
-  // Interpolate and repeat
+  // Create a particle
+  // Move it step by step
+  // Random direction
+  // Set interval
+  // Repeat forever
+
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, width, height);
+  context.lineWidth = 20;
+
+  // The first value of x,y will be the center
+  let penX = random.range(0, width);
+  let penY = random.range(0, height);
 
   // Render
-  return ({ context, width, height, time }) => {
+  return ({ context, width, height, playhead }) => {
     // Everything in here will be requested fps times per second
 
-    // Returns a random set of coordinates when called
-    const generateCoordinates = () => {
-      const randomX = random.range(margin, width - margin);
-      const randomY = random.range(margin, height - margin);
-      return [randomX, randomY];
-    };
+    // Restart after the duration is finished
+    if (playhead > 0.99) {
+      context.fillRect(0, 0, width, height);
+      penX = width / 2;
+      penY = height / 2;
+    }
+    // Add random value to previous value
+    // constrain distance of movement between -4 and 4
 
-    const draw = () => {
-      const { oldPoints, newPoints } = position;
-      let [u1, v1] = oldPoints;
-      let [u2, v2] = newPoints;
+    // We want to choose a random angle and move there instead
+    let newY = penY + random.range(-4, 4);
+    let newX = penX + random.range(-4, 4);
 
-      // Renew the white bg every frame
-      context.fillStyle = 'white';
-      // context.fillRect(0, 0, width, height);
-      context.stokeStyle = 'black';
+    // Wrap around boundaries of canvas
+    let wrapped = false;
+    if (newX > width) {
+      newX -= width;
+      wrapped = true;
+    }
+    if (newX < 0) {
+      newX += width;
+      wrapped = true;
+    }
+    if (newY > height) {
+      newY -= height;
+      wrapped = true;
+    }
+    if (newY < 0) {
+      newY += height;
+      wrapped = true;
+    }
 
-      // Old location circle
+    if (!wrapped) {
+      // draw from previous x,y to new x,y
       context.beginPath();
-      context.arc(u1, v1, 5, 0, Math.PI * 2, false);
+      context.moveTo(penX, penY);
+      context.lineWidth = 2;
+      context.strokeStyle = '#f05d05';
+      context.lineTo(newX, newY);
       context.stroke();
-      context.closePath();
+    }
 
-      // Line between points
-      context.beginPath();
-      context.moveTo(u1, v1);
-      context.lineTo(u2, v2);
-      context.stroke();
-
-      // New points circle
-      context.beginPath();
-      context.arc(u2, v2, 5, 0, Math.PI * 2, false);
-      context.stroke();
-      context.fill();
-      context.closePath();
-    };
-
-    // Get some coordnates at the beginning of the sketch
-    // We don't want to set a new position every frame
-    const position = {
-      oldPoints: generateCoordinates(),
-      newPoints: generateCoordinates(),
-    };
-
-    // Draw a singular line between the new and old coordinates
-    draw();
-
-    // Gather points inbetween old and new coordinates
-    // const x = lerp(u1, u2, time);
-    // const y = lerp(v1, v2, time);
-
-    // Moving circle
-    // context.beginPath();
-    // context.arc(x, y, 5, 0, Math.PI * 2, false);
-    // context.fill();
-    // context.closePath();
+    // save x,y for next frame of animation drawing
+    penX = newX;
+    penY = newY;
   };
 }, settings);

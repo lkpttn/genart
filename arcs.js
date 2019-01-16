9206dbabc9d102ffcb2ffb12f4f291a4b1f5d0ff
Luke Patton <lpatton@dosomething.org>
2019-01-16 17:30:26 -0500

    Started a circle made of beziers

--- arcs.js
+++ arcs.js
@@ -2,7 +2,7 @@ const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [514, 514],
+  dimensions: [500, 500],
 };
 
 const sketch = () => {
@@ -10,20 +10,45 @@ const sketch = () => {
   const lineWidth = 0.005;
   const count = 15;
 
+  // Squiggle line
   const generatePoints = () => {
     const points = [];
     for (let i = 0; i < count; i++) {
+      let offset = random.range(0, 100);
       points.push({
-        start: { x: 50 + i * 2, y: 20 + i * 2 },
-        control1: { x: 230 + i * 40, y: 30 + i * 40 },
-        control2: { x: 150 + i * 40, y: 80 + i * 40 },
-        end: { x: 250 + i * 40, y: 100 + i * 40 },
+        control1: { x: 40 * i, y: 55 * i },
+        control2: { x: 50 * i, y: 40 * i },
+        end: { x: 50 * i, y: 50 * i },
       });
     }
     return points;
   };
 
-  const points = generatePoints();
+  // Circle
+  const points = [
+    {
+      control1: { x: 350, y: 160 },
+      control2: { x: 250, y: 180 },
+      end: { x: 350, y: 250 },
+    },
+    {
+      control1: { x: 340, y: 355 },
+      control2: { x: 300, y: 320 },
+      end: { x: 250, y: 350 },
+    },
+    {
+      control1: { x: 200, y: 275 },
+      control2: { x: 150, y: 340 },
+      end: { x: 150, y: 250 },
+    },
+    {
+      control1: { x: 140, y: 155 },
+      control2: { x: 200, y: 200 },
+      end: { x: 250, y: 150 },
+    },
+  ];
+
+  // const points = generatePoints();
   console.log(points);
 
   // Render
@@ -34,8 +59,9 @@ const sketch = () => {
     context.strokeStyle = 'black';
     context.lineWidth = width * lineWidth;
 
+    // lines
     context.beginPath();
-    context.moveTo(100, 100);
+    context.moveTo(250, 150);
     // context.lineTo(point.start.x, point.start.y);
     for (let i = 0; i < points.length; i++) {
       const point = points[i];
@@ -49,6 +75,28 @@ const sketch = () => {
       );
     }
     context.stroke();
+
+    // End point
+    context.fillStyle = 'red';
+    for (let i = 0; i < points.length; i++) {
+      const point = points[i];
+      context.beginPath();
+      context.arc(point.end.x, point.end.y, 5, 0, Math.PI * 2, false);
+      context.fill();
+    }
+
+    // Control points
+    for (let i = 0; i < points.length; i++) {
+      const point = points[i];
+      context.beginPath();
+      context.fillStyle = 'blue';
+      context.arc(point.control1.x, point.control1.y, 3, 0, Math.PI * 2, false);
+      context.fill();
+      context.beginPath();
+      context.fillStyle = 'green';
+      context.arc(point.control2.x, point.control2.y, 3, 0, Math.PI * 2, false);
+      context.fill();
+    }
 
     // Showing the control points
     // context.fillStyle = 'red';

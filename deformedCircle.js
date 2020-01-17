ff4c886436bad4c25c4875f0899de1cb854942c8
Luke Patton <lpatton@dosomething.org>
2020-01-17 16:56:48 -0500

    Made some weird marbles with noise circles

--- deformedCircle.js
+++ deformedCircle.js
@@ -9,17 +9,47 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-    context.strokeStyle = 'black';
-    context.lineWidth = 3;
 
-    context.translate(width / 2, height / 2);
+    context.arc(width / 2, height / 2, width * 0.4, 0, Math.PI * 2, false);
+    context.clip();
 
-    for (let i = 0; i < 120; i++) {
-      drawDeformedCircle(50 + i * 10);
-    }
+    var colors = [
+      '#FFB713', // Gold
+      '#5200C5', // Purple
+      '#009F45', // Green
+      '#FFB084', // peach
+      '#007EBE', // sea blue
+      '#0035A0', // deep blue
+      '#892F54', // mauve
+      '#FF002B', // red
+      '#FFB084', // peach
+      '#007EBE', // sea blue
+      '#FF95BA', // pink
+    ];
+
+    context.globalCompositeOperation = 'exclusion';
 
     // Draw a deformed circle
-    function drawDeformedCircle(radius) {
+    for (let i = 0; i < 25; i++) {
+      fillDeformedCircle(
+        random.rangeFloor(0, width),
+        random.rangeFloor(0, height),
+        random.rangeFloor(200, 800 - i * 10),
+        random.range(0.3, 0.5),
+        0.1,
+        random.pick(colors),
+      );
+    }
+
+    function stampCircle(x, y, rings = 30, gap = 10, frequency, magnitude) {
+      for (let i = 0; i < rings; i++) {
+        drawDeformedCircle(x, y, 1 + i * gap, frequency, magnitude);
+      }
+    }
+
+    function fillDeformedCircle(x, y, radius, frequency, magnitude, color) {
+      context.save();
+      context.translate(x, y);
       context.beginPath();
       let points = Math.floor(4 * radius);
       for (let i = 0; i < points + 1; ++i) {
@@ -29,10 +59,37 @@ const sketch = () => {
         // Figure out the x/y coordinates for the given angle
         const x = Math.cos(angle);
         const y = Math.sin(angle);
-        const deformedRadius = radius + 100 * Math.abs(random.noise2D(x, y));
+        const deformation = Math.abs(
+          random.noise2D(x * frequency, y * frequency),
+        );
+        const deformedRadius = radius * (1 + magnitude * deformation);
+        context.lineTo(deformedRadius * x, deformedRadius * y);
+      }
+      context.fillStyle = color;
+      context.fill();
+      context.restore();
+    }
+
+    function drawDeformedCircle(x, y, radius, frequency, magnitude) {
+      context.save();
+      context.translate(x, y);
+      context.beginPath();
+      let points = Math.floor(4 * radius);
+      for (let i = 0; i < points + 1; ++i) {
+        // Is this in radians?
+        const angle = (2 * Math.PI * i) / points;
+
+        // Figure out the x/y coordinates for the given angle
+        const x = Math.cos(angle);
+        const y = Math.sin(angle);
+        const deformation = Math.abs(
+          random.noise2D(x * frequency, y * frequency),
+        );
+        const deformedRadius = radius * (1 + magnitude * deformation);
         context.lineTo(deformedRadius * x, deformedRadius * y);
       }
       context.stroke();
+      context.restore();
     }
   };
 };

5314715309668082bad2087fc94132761dd4f365
Luke Patton <lpatton@dosomething.org>
2020-02-07 11:55:30 -0500

    Flowfield

--- perlingrid/sketch.js
+++ perlingrid/sketch.js
@@ -1,29 +1,63 @@
-var increment = 0.02; // Amount of change
+var increment = 0.1; // Amount of change
+var scl = 10;
+var cols, rows;
+
+var zoff = 0;
+
+var fr;
+
+var particles = [];
+var flowField;
 
 function setup() {
   // put setup code here
   createCanvas(200, 200);
-  pixelDensity(1);
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  fr = createP('');
+
+  flowField = new Array(cols * rows);
+
+  for (let i = 0; i < 200; i++) {
+    particles[i] = new Particle();
+  }
 }
 
 function draw() {
-  loadPixels();
-
+  background(255);
   var yoff = 0;
-  for (let y = 0; y < height; y++) {
+  for (let y = 0; y < rows; y++) {
     var xoff = 0; // Reset xoffset for each row
-    for (let x = 0; x < width; x++) {
-      let index = (x + y * width) * 4;
-      var r = noise(xoff, yoff) * 255;
-      pixels[index + 0] = r;
-      pixels[index + 1] = 255;
-      pixels[index + 2] = 140;
-      pixels[index + 3] = 255;
+    for (let x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(0.1);
+
+      flowField[index] = v;
 
       xoff += increment;
+
+      stroke(0, 50);
+
+      // // Draw a bunch of lines
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // line(0, 0, scl, 0);
+      // strokeWeight(1);
+      // pop();
     }
     yoff += increment; // Increment after each row
+    zoff += 0.0003; // Increment over time
   }
 
-  updatePixels();
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].follow(flowField);
+    particles[i].update();
+    particles[i].show();
+    particles[i].edges();
+  }
+
+  fr.html(floor(frameRate()));
 }
